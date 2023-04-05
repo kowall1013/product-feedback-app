fe0adf2639c35059fdc4e1aa124fefe8bfcfb3cc
mateusz.kowalski <mkmotywacja@gmail.com>
2023-04-05 12:09:47 +0200

    create stroe

--- src/reduxState/store.ts
+++ src/reduxState/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sugestionsFeedbackSlice from "pages/suggestions/SugestionsSlice";
 
-console.log("jestem");
-
 export const store = configureStore({
-  reducer: {
-    feedbackList: sugestionsFeedbackSlice,
-  },
-  devTools: true,
+	reducer: {
+		feedbackList: sugestionsFeedbackSlice,
+	},
+	devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
