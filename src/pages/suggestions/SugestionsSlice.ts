d2687afde9ea168b75a099ceaed4535800a24578
mateusz.kowalski <mkmotywacja@gmail.com>
2023-04-06 09:18:10 +0200

    create filtering suggestions

--- src/pages/suggestions/SugestionsSlice.ts
+++ src/pages/suggestions/SugestionsSlice.ts
@@ -1,52 +1,69 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "reduxState/store";
+import { createSlice, current } from "@reduxjs/toolkit";
+import { LEAST_COMMENTS, LEAST_UPVOTES, MOST_COMMENTS, MOST_UPVOTES } from "pages/suggestions/navbarExtension/NavbarExtension";
 
 import Data from "store/data.json";
 
 interface User {
-  image: string;
-  name: string;
-  username: string;
+	image: string;
+	name: string;
+	username: string;
 }
 
 interface Replies {
-  content: string;
-  replyingTo: string;
-  user: User;
+	content: string;
+	replyingTo: string;
+	user: User;
 }
 
 interface Comment {
-  id: number;
-  content: string;
-  user: User;
-  replies?: Replies[];
+	id: number;
+	content: string;
+	user: User;
+	replies?: Replies[];
 }
 
-interface Feedback {
-  id: number;
-  title: string;
-  category: string;
-  upvotes: number;
-  status: string;
-  description: string;
-  comments?: Comment[];
+export interface Feedback {
+	id: number;
+	title: string;
+	category: string;
+	upvotes: number;
+	status: string;
+	description: string;
+	comments?: Comment[];
 }
 
 interface FeedbackListState {
-  currentUser: User;
-  productRequests: Feedback[];
+	currentUser: User;
+	productRequests: Feedback[];
 }
 
 const initialState: FeedbackListState = {
-  currentUser: Data.currentUser,
-  productRequests: Data.productRequests,
+	currentUser: Data.currentUser,
+	productRequests: Data.productRequests,
 };
 
 const sugestionsFeedbackSlice = createSlice({
-  name: "feedbackList",
-  initialState,
-  reducers: {},
+	name: "feedbackList",
+	initialState,
+	reducers: {
+		sortFeedbackList: (state, action) => {
+			let sugesstions = state.productRequests.filter((suggestion) => suggestion.status === "suggestion");
+
+			if (action.payload === MOST_UPVOTES) {
+				sugesstions.sort((a, b) => b.upvotes - a.upvotes);
+			} else if (action.payload === LEAST_UPVOTES) {
+				sugesstions.sort((a, b) => a.upvotes - b.upvotes);
+			} else if (action.payload === MOST_COMMENTS) {
+				sugesstions.sort((a, b) => (b.comments ? b.comments.length : 0) - (a.comments ? a.comments.length : 0));
+			} else if (action.payload === LEAST_COMMENTS) {
+				sugesstions.sort((a, b) => (a.comments ? a.comments.length : 0) - (b.comments ? b.comments.length : 0));
+			}
+
+			state.productRequests = sugesstions;
+		},
+	},
 });
 
 export default sugestionsFeedbackSlice.reducer;
+
+export const { sortFeedbackList } = sugestionsFeedbackSlice.actions;
